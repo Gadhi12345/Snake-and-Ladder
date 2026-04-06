56fa75e52ad409ada75249acaa827eea36e3d57d
Adarsh <adarshniharika20@gmail.com>
2026-04-06 12:48:49 +0530

    Initial Commit on UC3

--- GAME-SNAKE and LADDER/GAME-SNAKE and LADDER/Program.cs
+++ GAME-SNAKE and LADDER/GAME-SNAKE and LADDER/Program.cs
@@ -4,9 +4,29 @@ class Game
 {
     static void Main()
     {
+        int player1 = 0;
         Random random = new Random();
-        int dice = random.Next(1, 7);
 
-        Console.WriteLine("Dice rolled: " + dice);
+        int dice = random.Next(1, 7);
+        int option = random.Next(0, 3);
+
+        switch (option)
+        {
+            case 0:
+                Console.WriteLine("No Play");
+                break;
+
+            case 1:
+                Console.WriteLine("Ladder");
+                player1 += dice;
+                break;
+
+            case 2:
+                Console.WriteLine("Snake");
+                player1 -= dice;
+                break;
+        }
+
+        Console.WriteLine("Position: " + player1);
     }
 }
