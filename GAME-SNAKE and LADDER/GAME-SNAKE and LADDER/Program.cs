c36006797d324a9f6652dc5ee252c911ec0a4839
Adarsh <adarshniharika20@gmail.com>
2026-04-06 13:56:07 +0530

    Initial Commit on UC7

--- GAME-SNAKE and LADDER/GAME-SNAKE and LADDER/Program.cs
+++ GAME-SNAKE and LADDER/GAME-SNAKE and LADDER/Program.cs
@@ -1,40 +1,55 @@
 ﻿using System;
 
-class Game
+class UC7
 {
     static void Main()
     {
         int player1 = 0;
-        int count = 0;
+        int player2 = 0;
+        int currentPlayer = 1;
+
         Random random = new Random();
 
-        while (player1 < 100)
+        while (player1 < 100 && player2 < 100)
         {
             int dice = random.Next(1, 7);
             int option = random.Next(0, 3);
 
-            count++;
+            Console.WriteLine("\nPlayer " + currentPlayer + " rolled: " + dice);
 
-            int temp = player1;
+            int temp = (currentPlayer == 1) ? player1 : player2;
 
             switch (option)
             {
                 case 0:
+                    Console.WriteLine("No Play");
                     break;
+
                 case 1:
+                    Console.WriteLine("Ladder");
                     temp += dice;
                     break;
+
                 case 2:
+                    Console.WriteLine("Snake");
                     temp -= dice;
                     break;
             }
 
             if (temp >= 0 && temp <= 100)
-                player1 = temp;
+            {
+                if (currentPlayer == 1)
+                    player1 = temp;
+                else
+                    player2 = temp;
+            }
 
-            Console.WriteLine("Roll: " + count + " Position: " + player1);
+            Console.WriteLine("P1: " + player1 + " | P2: " + player2);
+
+            if (option != 1)
+                currentPlayer = (currentPlayer == 1) ? 2 : 1;
         }
 
-        Console.WriteLine("Total Rolls: " + count);
+        Console.WriteLine((player1 == 100) ? "Player 1 Wins!" : "Player 2 Wins!");
     }
 }
